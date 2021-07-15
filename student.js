3cbbe36878d289229e09f85ce4ab8cd7d618bc98
SunVishnu7 <87486045+SunVishnu7@users.noreply.github.com>
2021-07-15 19:50:49 +0530

    Add files via upload

---
+++ student.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAvNLq5798E1cL7OBb3hA3lXb4xnKFkkIA",
+    authDomain: "school-chat-f10cb.firebaseapp.com",
+    projectId: "school-chat-f10cb",
+    storageBucket: "school-chat-f10cb.appspot.com",
+    messagingSenderId: "1069996150074",
+    appId: "1:1069996150074:web:7ce654235c875c211a1403"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
